e6e2bc7b3cc786190f7e115302a0028a0a771ba9
skullquake <ockert8080@gmail.com>
2019-10-21 10:53:12 -0700

    .

--- src/flsk_mt_006/app/routes.py
+++ src/flsk_mt_006/app/routes.py
@@ -36,23 +36,13 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts = [
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
+	posts=current_user.followed_posts().all()
 	return render_template(
 		"index.html",
 		title='Home Page',
 		form=form,
 		posts=posts
 	)
-
 @app.route('/login',methods=['GET','POST'])
 def login():
 	if current_user.is_authenticated:
@@ -142,3 +132,13 @@ def unfollow(username):
 	db.session.commit()
 	flash('You are not following {}.'.format(username))
 	return redirect(url_for('user', username=username))
+@app.route('/explore')
+@login_required
+def explore():
+	posts=Post.query.order_by(Post.timestamp.desc()).all()
+	return render_template(
+		'index.html',
+		title='Explore',
+		posts=posts
+	)
+
