20e6b38dd486cb40fe7c30bc456a86a167fd22f2
skullquake <ockert8080@gmail.com>
2019-10-22 06:11:22 -0700

    .

--- src/flsk_mt_006/app/routes.py
+++ src/flsk_mt_006/app/routes.py
@@ -36,12 +36,25 @@ def index():
 		db.session.commit()
 		flash('Your post is now live!')
 		return redirect(url_for('index'))
-	posts=current_user.followed_posts().all()
+	page=request.args.get(
+		'page',
+		1,
+		type=int
+	)
+	posts=current_user.followed_posts().paginate(
+		page,
+		app.config['POSTS_PER_PAGE'],
+		False
+	)
+	next_url=url_for('index',page=posts.next_num)if posts.has_next else None
+	prev_url=url_for('index',page=posts.prev_num)if posts.has_prev else None
 	return render_template(
 		"index.html",
 		title='Home Page',
 		form=form,
-		posts=posts
+		posts=posts.items,
+		next_url=next_url,
+		prev_url=prev_url
 	)
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -75,7 +88,11 @@ def register():
 		db.session.commit()
 		flash('Congratulations, you are now a registered user!')
 		return redirect(url_for('login'))
-	return render_template('register.html', title='Register', form=form)
+	return render_template(
+		'register.html',
+		title='Register',
+		form=form
+	)
 @app.route('/user/<username>')
 @login_required
 def user(username):
@@ -84,7 +101,25 @@ def user(username):
 		{'author':user,'body':'Test post #1'},
 		{'author':user,'body':'Test post #2'}
 	]
-	return render_template('user.html',user=user,posts=posts)
+	page=request.args.get(
+		'page',
+		1,
+		type=int
+	)
+	posts=user.posts.order_by(Post.timestamp.desc()).paginate(
+		page,
+		app.config['POSTS_PER_PAGE'],
+		False
+	)
+	next_url=url_for('user',username=user.username,page=posts.next_num)if posts.has_next else None
+	prev_url=url_for('user',username=user.username,page=posts.prev_num)if posts.has_prev else None
+	return render_template(
+		'user.html',
+		user=user,
+		posts=posts.items,
+		next_url=next_url,
+		prev_url=prev_url,
+	)
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
@@ -135,9 +170,33 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-	posts=Post.query.order_by(Post.timestamp.desc()).all()
+	page=request.args.get(
+		'page',
+		1,
+		type=int
+	)
+	posts=Post.query.order_by(
+		Post.timestamp.desc()
+	).paginate(
+		page,app.config['POSTS_PER_PAGE'],
+		False
+	)
+	page=request.args.get(
+		'page',
+		1,
+		type=int
+	)
+	posts=Post.query.order_by(Post.timestamp.desc()).paginate(
+		page,
+		app.config['POSTS_PER_PAGE'],
+		False
+	)
+	next_url=url_for('explore',page=posts.next_num)if posts.has_next else None
+	prev_url=url_for('explore',page=posts.prev_num)if posts.has_prev else None
 	return render_template(
 		'index.html',
 		title='Explore',
-		posts=posts
+		posts=posts.items,
+		next_url=next_url,
+		prev_url=prev_url
 	)
---
+++ src/flsk_mt_007/config.py
@@ -0,0 +1,22 @@
+import os
+basedir=os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+	SECRET_KEY=os.environ.get('SECRET_KEY')or'you-will-never-guess'
+	STATIC_FOLDER="app/static"
+	STATIC_PATH='app/static'
+	SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS=False
+
+	ADMINS=['ockert8080@gmail.com']
+
+	#MAIL_SERVER='localhost'
+	#MAIL_PORT=8025
+	#MAIL_USE_TLS=False
+
+	MAIL_SERVER='smtp.gmail.com'
+	MAIL_PORT=587
+	MAIL_USE_TLS=1
+	MAIL_USERNAME='ockert8080@gmail.com'
+	MAIL_PASSWORD='4321$#@!rewqREWQ'
+
+	POSTS_PER_PAGE=5
