c6bd86bc75e90a2f8491a7e72fc7c9d1b5debc03
skullquake <ockert8080@gmail.com>
2019-10-22 07:24:06 +0200

    .

--- src/flsk_mt_006/app/routes.py
+++ src/flsk_mt_006/app/routes.py
@@ -31,7 +31,7 @@ from datetime import\
 def index():
 	form=PostForm()
 	if form.validate_on_submit():
-		post = Post(body=form.post.data, author=current_user)
+		post=Post(body=form.post.data,author=current_user)
 		db.session.add(post)
 		db.session.commit()
 		flash('Your post is now live!')
@@ -141,4 +141,3 @@ def explore():
 		title='Explore',
 		posts=posts
 	)
-
